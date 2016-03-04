0a3c59c1e38a775521ab033c5261dec6c0e14739
myh1000 <myh1000@gmail.com>
2016-03-04 14:25:22 -0800

    ez commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Nyaa downloader
+
+i stole this from somewhere
+this script search the rss feeds of nyaa.se using a file passed in parameter and download the torrents files to a specified directory. Useful to auto add new anime episode to a torrent client.
+
+## How to use
+First create a text file that you will fill with what you want to search.
+
+Every line of the file will looks something like this:
+
+```my anime name;720p;HorribleSubs```
+
+This line for example would search for every rss item and download the torrent file when the item contains "my anime name" , "720p" AND "HorribleSubs". You can have as many line as you want in the file. There is also no limit to how much search terms you can put by line.
+
+Once your file is written you can launch the script with:
+
+```./nyaa_dl.rb file.txt /directory```
+
+You should probably put it in a cronjob or something so it would run a couple of times a day and download your new episode of "my anime name" when it comes out.
+
+like
+
+```0 15 * * * ruby ~/ur directory/nyaa_dl.rb ~/ur directory/anime.txt ~/anotha one/animu/```
+
+## Notes
+nyaa_dl search only for the last ≈100 torrents added to nyaa.se so this script is only useful for getting new episodes of ongoing animes.
+
+Logs are in nyaa.txt.
---
+++ nyaa_dl.rb
@@ -0,0 +1,82 @@
+#! /usr/bin/env ruby
+
+require 'rss'
+require 'open-uri'
+require 'net/http'
+
+# Argument verification
+if ARGV.length != 2
+  puts "Usage: #{$0} file.txt /directory/to/dump/torrents"
+  puts "file example:"
+  puts "anime name 1;1080;HorribleSubs"
+  puts "anime name 2;720;HorribleSubs"
+  exit 1
+end
+
+def main()
+  #reading the file passed in parameter
+  file = ARGV[0]
+  if ! File.exist?("#{file}")
+    puts "#{file} do not exist..."
+    exit 1
+  end
+  file = File.new("#{file}", "r")
+
+  # search the RSS feeds once for every line in the file
+  while (line = file.gets)
+      search_strings = line.split(";")
+      search_strings[-1] = search_strings[-1].chomp
+      search_rss(search_strings)
+  end
+end
+
+def search_rss(search_strings)
+  nyaa_rss_url = "http://www.nyaa.se/?page=rss"
+
+  open(nyaa_rss_url) do |rss|
+    feed = RSS::Parser.parse(rss)
+    feed.items.each do |item|
+      # Searching the item for every search_strings
+      found = 0
+      search_strings.each do |search_string|
+        if item.title.match(/(#{search_string})/)
+          found = found + 1
+        end
+      end
+
+      # If we found every search terms download the torrent
+      time1 = Time.new
+      if found == search_strings.length
+        download_torrent(item.link, item.title)
+        f = File.open('Documents/myh1000/animu/nyaa_dl/nyaa.txt', 'a')
+
+        old_out = $stdout
+        $stdout = f
+
+        puts "#{item.title}    " + time1.inspect
+
+        f.close
+
+        $stdout = old_out
+        puts "Downloading #{item.title}."
+      end
+    end
+  end
+end
+
+def download_torrent(link, torrent_name)
+  path = ARGV[1]
+
+  # Removing everything before www
+  link = link.gsub!(/.*?(?=www)/im, "")
+
+  # Downloading the torrent
+  Net::HTTP.start("#{link.split('/', 2).first}") do |http|
+    resp = http.get("/#{link.split('/', 2).last}")
+    open("#{path}/#{torrent_name}.torrent", "wb") do |file|
+      file.write(resp.body)
+    end
+  end
+end
+
+main()
